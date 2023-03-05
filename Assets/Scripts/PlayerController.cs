aebfb41b2619604ccf8fac0d01698628f4d7bdcd
Shardul Deodhar <sdeodhar@usc.edu>
2023-03-04 18:42:48 -0800

    analytics 5 updated

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -234,6 +234,8 @@ public class PlayerController : MonoBehaviour
             _time_taken = 120f - PlayerController.totalTime;
             ob.Send(other.gameObject.name, _time_taken - prev_time, level_name, _sessionId);
             other.gameObject.SetActive(false);
+                        SendAnalytics4 ob2 = gameObject.AddComponent<SendAnalytics4>();
+                        ob2.Send("Level completed without dying", level_name);
         }
     }
 
