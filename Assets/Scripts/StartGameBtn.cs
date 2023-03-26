2fb332e1b03a45d5e49f0465689cb521b9a317ab
Ashutosh Tiwari <ashutoshtiwari3309@gmail.com>
2023-03-25 17:26:37 -0700

    FIX: Level selector-lvl1to8

--- Assets/Scripts/StartGameBtn.cs
+++ Assets/Scripts/StartGameBtn.cs
@@ -24,11 +24,31 @@ public class StartGameBtn : MonoBehaviour
 
     public void Level2Start() {
         PlayerController.totalTime = 150;
-        SceneManager.LoadScene("FinalLvl2");
+        SceneManager.LoadScene("Prem Level");
     }
 
     public void Level3Start() {
         PlayerController.totalTime = 150;
-        SceneManager.LoadScene("FinalLvl3");
+        SceneManager.LoadScene("PremLevel2");
+    }
+
+    public void Level4Start() {
+        PlayerController.totalTime = 150;
+        SceneManager.LoadScene("Parth01");
+    }
+
+    public void Level5Start() {
+        PlayerController.totalTime = 150;
+        SceneManager.LoadScene("Parth02");
+    }
+
+    public void Level6Start() {
+        PlayerController.totalTime = 150;
+        SceneManager.LoadScene("PremLevel3");
+    }
+
+    public void Level7Start() {
+        PlayerController.totalTime = 150;
+        SceneManager.LoadScene("Parth03");
     }
 }
