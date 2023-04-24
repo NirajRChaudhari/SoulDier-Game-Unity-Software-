8dc4d88d514dc2c9422d814065d1ff058552bb8d
Niraj Ramakant Chaudhari <nirajchaudhari8698@gmail.com>
2023-04-23 17:29:09 -0700

    Spring scripts fixed

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -7,17 +7,32 @@ public class JumpPad : MonoBehaviour
 {
 
     private float jumpPower = 20f;
-
-    
+    private float alpha = 0.5f;
+    private SpriteRenderer jumpPadSpriteRenderer;
+    private Color[] colors;
+    private int colorIndex=0;
+    Color red, blue, yellow, black;
 
 
     private void Start() {
-
+        
+        red = Color.red;
+        blue = Color.blue;
+        yellow = Color.yellow;
+        black = new Color(0, 0, 0, 1);
+        colors = new Color[] { red, yellow, blue };
+        gameObject.layer = 3;
+    jumpPadSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    InvokeRepeating("ChangeColor", 0.0f, 4.0f);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        SpriteRenderer collisionSprite = collision.gameObject.GetComponent<SpriteRenderer>();
+
+        bool condition = (jumpPadSpriteRenderer.color.r == collisionSprite.color.r )&&(jumpPadSpriteRenderer.color.g == collisionSprite.color.g )&&(jumpPadSpriteRenderer.color.b == collisionSprite.color.b );
+
+        if (collision.gameObject.CompareTag("Player") && condition)
         {
             try{
                 PlayerController otherInstance = FindObjectOfType<PlayerController>();
@@ -33,5 +48,12 @@ public class JumpPad : MonoBehaviour
 
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
         }
+    }
+
+        void ChangeColor()
+    {
+            colorIndex = (colorIndex) % colors.Length;
+            jumpPadSpriteRenderer.color = colors[colorIndex];
+            colorIndex++;   
     }
 }
